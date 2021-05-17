f453e84876029109564de6be47e5206481080980
Sergi-P <sergipifarre@gmail.com>
2021-05-17 16:55:03 +0200

    pruebas en el webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,43 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const devMode = process.env.NODE_ENV !== 'production'
 
-
 module.exports = {
-  entry: //'./index_.html',
-  './frontend/app.js',
+  entry: './index_.html',
+  mode: 'development',
+
+  module : {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  }/*,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './frontend/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/bundle.css"
+    })
+  ],
+  devtool: 'source-map'*/
+};
+
+/*
+module.exports = {
+  entry: './frontend/app.js',
   mode: 'development',
   output: {
     path: path.join(__dirname, 'backend/public'),
@@ -41,4 +74,4 @@ module.exports = {
     })
   ],
   devtool: 'source-map'
-};
+};*/
